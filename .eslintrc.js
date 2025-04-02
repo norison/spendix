36f5c6d7463c66b68839e4ef2a37ff9ee5cef7ad
Ihor Matiev <ihor.matiev@gmail.com>
2025-04-03 00:53:04 +0200

    Added jest plugin for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,17 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
   extends: ["expo", "prettier"],
-  plugins: ["prettier"],
+  plugins: ["prettier", "jest"],
   ignorePatterns: ["/dist/*"],
+  env: {
+    "jest/globals": true,
+  },
   rules: {
     "prettier/prettier": "error",
-  },
-  env: {
-    jest: true,
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error",
   },
 };
---
+++ tests/MonoText.spec.tsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react-native";
+import { MonoText } from "@/components/StyledText";
+
+describe("MonoText", () => {
+  it("renders correctly", () => {
+    const text = "Hello World";
+    const { getByText } = render(<MonoText>{text}</MonoText>);
+    expect(getByText(text)).toBeTruthy();
+  });
+});
