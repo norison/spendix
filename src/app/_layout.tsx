395dcb4b12f1eda4362f9c877df96f0cd785a8eb
Ihor Matiev <48079411+norison@users.noreply.github.com>
2025-04-17 10:47:27 +0200

    Feat/ci (#19)
    * Add basic screens and ci action
    
    * Fix spinner formatting

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,10 @@
 import "@/global.css";
 import { FC } from "react";
 import { GluestackUIProvider } from "@/src/components/ui/gluestack-ui-provider";
-import { Tabs } from "expo-router";
+import { Slot } from "expo-router";
 import { useColorScheme } from "nativewind";
 import { StatusBar } from "expo-status-bar";
 import { Box } from "@/src/components/ui/box";
-import { Heading } from "@/src/components/ui/heading";
-import { Text } from "@/src/components/ui/text";
-import { GlobeIcon, Icon, SettingsIcon } from "@/src/components/ui/icon";
-import { Platform } from "react-native";
 
 const RootLayout: FC = () => {
   const { colorScheme } = useColorScheme();
@@ -16,55 +12,9 @@ const RootLayout: FC = () => {
   return (
     <GluestackUIProvider mode={colorScheme}>
       <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
-      <Tabs
-        screenOptions={{
-          headerBackground: () => (
-            <Box className="w-full h-full bg-background-0" />
-          ),
-          tabBarBackground: () => (
-            <Box className="w-full h-full bg-background-0" />
-          ),
-          tabBarStyle: {
-            borderTopWidth: 0,
-            elevation: 0,
-            position: "absolute",
-            bottom: Platform.OS === "android" ? 15 : 0,
-          },
-          headerTitle: ({ children }) => <Heading>{children}</Heading>,
-          tabBarLabel: ({ children, focused }) => (
-            <Text className={focused ? "text-tertiary-600" : "text-primary-0"}>
-              {children}
-            </Text>
-          ),
-        }}
-      >
-        <Tabs.Screen
-          name="index"
-          options={{
-            title: "Home",
-            tabBarIcon: ({ focused }) => (
-              <Icon
-                size="xl"
-                className={focused ? "text-tertiary-600" : "text-primary-0"}
-                as={GlobeIcon}
-              />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="settings"
-          options={{
-            title: "Settings",
-            tabBarIcon: ({ focused }) => (
-              <Icon
-                size="xl"
-                className={focused ? "text-tertiary-600" : "text-primary-0"}
-                as={SettingsIcon}
-              />
-            ),
-          }}
-        />
-      </Tabs>
+      <Box className="bg-background-0 w-full h-full flex justify-center items-center">
+        <Slot />
+      </Box>
     </GluestackUIProvider>
   );
 };
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,25 +1,8 @@
 import { FC } from "react";
-import { Button, ButtonText } from "@/src/components/ui/button";
-import { Box } from "@/src/components/ui/box";
-import { Text } from "@/src/components/ui/text";
-import { useColorScheme } from "nativewind";
+import { Redirect } from "expo-router";
 
 const Index: FC = () => {
-  const { colorScheme, setColorScheme } = useColorScheme();
-
-  return (
-    <Box className="bg-background-0 w-full h-full flex justify-center items-center">
-      <Text size="4xl">Hello</Text>
-      <Button
-        className="mt-5"
-        onPress={() => {
-          setColorScheme(colorScheme === "light" ? "dark" : "light");
-        }}
-      >
-        <ButtonText>Toggle color mode</ButtonText>
-      </Button>
-    </Box>
-  );
+  return <Redirect href="auth/signin" />;
 };
 
 export default Index;
